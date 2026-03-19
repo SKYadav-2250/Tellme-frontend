06ead4ae36bb7f30226871c21afe7ee9ddd31758
SKYadav-2250 <skyadav225075@gmail.com>
2026-03-19 23:41:25 +0530

    remove localhost

--- src/runtimeConfig.js
+++ src/runtimeConfig.js
@@ -1,14 +1,9 @@
 const trimTrailingSlash = (value = '') => value.replace(/\/+$/, '')
 
-const DEFAULT_DEV_BACKEND_URL = 'http://localhost:5000'
-const DEFAULT_PROD_BACKEND_URL = 'https://tell-me-backend.onrender.com'
 
-const fallbackBackendUrl = import.meta.env.DEV
-  ? DEFAULT_DEV_BACKEND_URL
-  : DEFAULT_PROD_BACKEND_URL
 
 export const API_BASE_URL = trimTrailingSlash(
-  import.meta.env.VITE_API_URL || fallbackBackendUrl,
+  import.meta.env.VITE_API_URL,
 )
 
 export const SOCKET_BASE_URL = trimTrailingSlash(
